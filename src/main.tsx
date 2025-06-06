6303242e069acefd3cca474c90702dc1cba70dae
Shancara <shanniecarmi@gmail.com>
2025-06-06 07:00:13 +0300

    Describe your change concisely

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById('root')!).render(
   // Temporarily removed StrictMode to debug
-  <App />
+  <>
+    <App />
+    <SpeedInsights />
+  </>
 )
